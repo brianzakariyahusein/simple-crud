742c17c8dc5f6ae6752ceb4c860db52897416564
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-26 20:04:54 +0700

    Membuat user model

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/simple_crud");
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Database Connected..."));
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(5000, () => console.log("Server up and running..."));
