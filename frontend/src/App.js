5374fc15478564f8a27ef7c3b16b03e36e4151fd
Brian Zakariya <zakariyabrian@gmail.com>
2025-03-26 22:30:20 +0700

    Setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import UserList from "./components/UserList";
-import AddUser from "./components/AddUser";
-import EditUser from "./components/EditUser";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<UserList />} />
-          <Route path="add" element={<AddUser />} />
-          <Route path="edit/:id" element={<EditUser />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
